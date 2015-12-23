51f8a96d883c2d35adfdf60ce2177801de7fc5aa
Konstantin Gredeskoul <kigster@gmail.com>
2015-12-22 16:50:21 -0800

    Decreasing default version until homebrew updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ node.default['sprout']['pyenv'] = {
   'home' => "#{node['sprout']['home']}/.pyenv",
   'command' => '/usr/local/bin/pyenv',
   'pythons' => {
-      '2.7.11' => {}
+      '2.7.10' => {}
     },
-  'default_python' => '2.7.11',
+  'default_python' => '2.7.10',
 }
